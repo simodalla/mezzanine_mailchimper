61f153f7ac2306a89a59f4030f344fc012ce5869
Simone Dalla <simodalla@gmail.com>
2014-03-06 16:27:31 +0100

    move from mixin to model manager

--- mailchimper/utils.py
+++ mailchimper/utils.py
@@ -1,19 +1,2 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-
-from django.core.exceptions import ImproperlyConfigured
-
-from mezzanine.conf import settings
-
-from mailchimp import Mailchimp
-
-
-class MailchimperModel(object):
-
-    @property
-    def mailchimper(self):
-        settings.use_editable()
-        if not getattr(settings, 'MAILCHIMP_API_KEY', None):
-            raise ImproperlyConfigured("The MAILCHIMP_API_KEY setting must "
-                                       "not be empty.")
-        return Mailchimp(settings.MAILCHIMP_API_KEY)
